856b9ed0962b604ea52bd5b63f0fd225ca24660a
MrSullivan <>
2021-04-20 10:58:45 +0000

    first and second variables set, if, elif and else statements comparing and sting commands with print.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    first = int(input(""))
+    second = int(input(""))
+    
+    if (first>second):
+      print(str(first) + " is greater than " + str(second) +".")
+    elif (first<second):
+      print(str(first) + " is smaller than " + str(second) + ".")
+    else:
+      print(str(first) + " is equal to " + str(second) + ".")
+
+
 
 if __name__ == '__main__':
     main()
